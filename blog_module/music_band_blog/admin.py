9a42644bcfdfda3dde9927c7cbd5d5ac7078a16d
green-fox <schreiber.alexis.as@gmail.com>
2014-10-15 00:06:48 +0200

    add article to  the admin page

--- blog_module/music_band_blog/admin.py
+++ blog_module/music_band_blog/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from music_band_blog.models import Video
+from music_band_blog.models import Video,Article
 
 class VideoAdmin(admin.ModelAdmin):
     fields = ['title','video_player' ]
     list_display = ('title','video_player','pub_date','updated')
 admin.site.register(Video,VideoAdmin)
+admin.site.register(Admin)
