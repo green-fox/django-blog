0ffea9debb072861af62c026514410459a534556
System Administrator <root@pb-d-128-141-246-107.cern.ch>
2014-10-14 14:35:04 +0200

    add the pub_date and update to the admin view

--- blog_module/music_band_blog/admin.py
+++ blog_module/music_band_blog/admin.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 from django.contrib import admin
 from music_band_blog.models import Video
 
+class VideoAdmin(admin.ModelAdmin):
+    fields = ['title','type','pub_date','updated' ]
 
-admin.site.register(Video)
+admin.site.register(Video,VideoAdmin)
