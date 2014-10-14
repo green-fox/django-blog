546441567de3ee4bbb2e0f367981da9810c68ecc
System Administrator <root@pb-d-128-141-246-107.cern.ch>
2014-10-14 12:31:41 +0200

    created a video model

--- blog_module/music_band_blog/models.py
+++ blog_module/music_band_blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+#class Article(models.Model):
+    title = models.CharField(max_length=200)
+
+#class Picture(models.Model):
+    
+
+class Video(models.Model):
+    YOUTUBE = 'YOUTUBE'
+    DAILYMOTION = 'DAILYMOTION'
+    VIMEO = 'VIMEO'
+    VIDEO_PLAYER_CHOICES= (
+        (YOUTUBE, 'youtube'),
+        (DAILYMOTION, 'dailymotion'),
+        (VIMEO,'vimeo'),
+    )
+    video_player = (max_length = 1,
+                    choices = VIDEO_PLAYER_CHOICES,
+                    default = YOUTUBE)
+    title = models.CharField(max_length=200)
+
+#class Concert(models.Model):
